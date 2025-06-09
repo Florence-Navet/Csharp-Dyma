a42caaf88ffe621580ddcad148232e7f5c771dec
Florence <enolaworld@gmail.com>
2025-06-09 20:34:44 +0200

    feat: modification fichier

--- Cours/Bases/chaineinterpolees.cs
+++ Cours/Bases/chaineinterpolees.cs
@@ -3,30 +3,30 @@
 //Console.WriteLine("Bonjour ! Comment t'appelles-tu ?");
 
 //pour recupérer données utilisateur
-//string? rep = Console.ReadLine();
+string? rep = Console.ReadLine();
 
-//Console.WriteLine("Bonjour " + rep + "!");
+Console.WriteLine("Bonjour " + rep + "!");
 
 
 //Console.WriteLine("Quel est ton année de naissance ?");
 
 
-//string repAnn = Console.ReadLine();
+string repAnn = Console.ReadLine();
 
-//int anneeNais = int.Parse(repAnn);
+int anneeNais = int.Parse(repAnn);
 
-//int age = 2024 - anneeNais;
+int age = 2024 - anneeNais;
 
-//int age = DateTime.Today.Year - anneeNais;
+int age = DateTime.Today.Year - anneeNais;
 
-//Console.WriteLine(anneeNais);
+Console.WriteLine(anneeNais);
 
-//Console.WriteLine("Tu vas avoir " + age + " ans.");
+Console.WriteLine("Tu vas avoir " + age + " ans.");
 
-//Console.WriteLine($"{rep},tu vas avoir {age} ans en {DateTime.Today.Year}.");
+Console.WriteLine($"{rep},tu vas avoir {age} ans en {DateTime.Today.Year}.");
 
 
-   
+
 
 
 
--- Cours/Bases/variables.cs
+++ Cours/Bases/variables.cs
@@ -1,23 +1,23 @@
 ﻿
 //le ? veut dire que la valeur est nullable est accepte la valeur nulle
-//int? scoreJ1M2 = null;
+int? scoreJ1M2 = null;
 
-//Console.WriteLine(scoreJ1M2);
+Console.WriteLine(scoreJ1M2);
 
-//scoreJ1M2 = 0;
+scoreJ1M2 = 0;
 
-//Console.WriteLine(scoreJ1M2);
+Console.WriteLine(scoreJ1M2);
 
-//string mot = "choupette";
+string mot = "choupette";
 
-//if (mot != null)
-//{
-//    string s2 = mot.ToUpper();
+if (mot != null)
+{
+    string s2 = mot.ToUpper();
 
-//    Console.WriteLine(s2);
+    Console.WriteLine(s2);
 
-//}
-//string s = string.Empty;
+}
+string s = string.Empty;
 
 
 
