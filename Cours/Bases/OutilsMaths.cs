071ef601275881fc941785998fbc194901528d0b
Florence <enolaworld@gmail.com>
2025-06-11 14:19:18 +0200

    getvaleursmax

--- Cours/Bases/OutilsMaths.cs
+++ Cours/Bases/OutilsMaths.cs
@@ -1,31 +1,51 @@
 ﻿using System;
 
-
-namespace Foncions
+#pragma warning disable IDE0130 // Le namespace ne correspond pas à la structure de dossiers
+namespace Fonctions
 {
     internal class OutilMaths
     {
+        // Méthode pour récupérer uniquement la valeur maximale
         public static double GetValeurMaxi(params double[] nombres)
         {
             double max = double.MinValue;
-            for (int i = 0; i < nombres.Length; i++)
+            foreach (double nombre in nombres)
             {
-                if (nombres[i] > max) max = nombres[i];
+                if (nombre > max) max = nombre;
             }
             return max;
         }
-        public static (double, double) GetValeurMaxi(params double[] nombres)
+
+        // Méthode renommée pour obtenir à la fois min et max
+        public static (double, double) GetValeursMinMax(params double[] nombres)
         {
             double min = double.MaxValue;
             double max = double.MinValue;
+
             foreach (double nombre in nombres)
             {
                 if (nombre > max) max = nombre;
                 if (nombre < min) min = nombre;
             }
+
             return (min, max);
         }
+
+        public static void GetValeursMinMax(out double min, out double max, params double[] nombres)
+        {
+            min = double.MaxValue;
+            max = double.MinValue;
+
+            foreach (double nombre in nombres)
+            {
+                if (nombre > max) max = nombre;
+                if (nombre < min) min = nombre;
+            }
+
+   
+        }
+
+
+
     }
-
-
 }
---
+++ Cours/Bases/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Fonctions;
+using Bidule;
+using System.Drawing;
+
+namespace Fonctions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+           Console.WriteLine("=== Test des fonctions sets de jeu ===");
+            WhileFor wf = new WhileFor(); // créer une instance
+            wf.Run();
+            Console.WriteLine("=== Test des fonctions OutilMaths ===");
+
+            double max = OutilMaths.GetValeurMaxi(1.2, 5.4, 3.3);
+            Console.WriteLine($"Valeur maximale : {max}");
+
+            (double min, double max2) = OutilMaths.GetValeursMinMax(1.2, 5.4, 3.3);
+            Console.WriteLine($"Valeur minimale : {min}, Valeur maximale : {max2}");
+
+            (double x, double y, double z) point = (2.2, 3.3, 4.4);
+            Console.WriteLine($"{point.x}, {point.y}, {point.y}");
+
+            //appelle nouvelle double GetValeursMinMax
+            OutilMaths.GetValeursMinMax(out double mini, out double maxi, 1.2, 3.7, 5.8, 9.4);
+            Console.WriteLine($"valeurs mini et maxi sont {mini} et {maxi}.");
+
+        }
+    }
+}
