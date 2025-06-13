183aa3ca652c44e58df1e81b5223c774b1d381df
Florence Navet <enolaworld@gmail.com>
2025-06-13 18:07:12 +0200

    feat: calcul moyenne meteo

--- Exercise/ExercicesCS/RelevesMeteo/Program.cs
+++ Exercise/ExercicesCS/RelevesMeteo/Program.cs
@@ -3,35 +3,32 @@
 namespace RelevesMeteo
 {
     internal class Program
-    //{
+    {
         static void Main()
         {
             Console.WriteLine("Bienvenue dans la météo !");
 
-            string cheminFichier = "MeteoParis.csv"; 
+            string cheminFichier = "MeteoParis.csv";
 
             if (File.Exists(cheminFichier))
             {
-                AfficherListe(cheminFichier); 
+                AfficherListe(cheminFichier);
             }
             else
             {
-                Console.WriteLine(" Fichier introuvable !");
+                Console.WriteLine("Fichier introuvable !");
             }
         }
 
-
-
         public static string AfficheData(string[] valeurs)
         {
-            string date = valeurs[0] + "/" + valeurs[1]; 
+            string date = valeurs[0] + "/" + valeurs[1];
             string temperatures = "[" + valeurs[2] + " ; " + valeurs[3] + "]°C";
-            string soleil = valeurs[5].Replace(" ", ""); 
+            string soleil = valeurs[5].Replace(" ", "");
             string pluie = valeurs[6] + " mm de pluie";
 
             return $"{date} : {temperatures}        {soleil} de soleil {pluie}";
         }
-
 
         public static void AfficherListe(string cheminFichier)
         {
@@ -43,15 +40,33 @@ namespace RelevesMeteo
 
             string[] lignes = File.ReadAllLines(cheminFichier);
 
-         
+            double SommeTemp = 0;
+            int NbValeurs = 0;
+
             for (int i = 1; i < lignes.Length; i++)
             {
-                string[] valeurs = lignes[i].Split(';'); 
-                string resultat = AfficheData(valeurs);  
+                string[] valeurs = lignes[i].Split(';');
+                if (double.TryParse(valeurs[2], out double tMin) && double.TryParse(valeurs[3], out double tMax))
+                {
+                    SommeTemp += tMin + tMax;
+                    NbValeurs += 2;
+                }
+                string resultat = AfficheData(valeurs);
 
                 Console.WriteLine(resultat);
             }
+            if (NbValeurs > 0)
+            {
+                double moyenne = SommeTemp / NbValeurs;
+                Console.WriteLine($"Température moyenne égale : {moyenne}");
+
+            } else
+            {
+                Console.WriteLine("Aucune température valide trouvée.");
+            }
         }
+       
+
 
     }
 }
