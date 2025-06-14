6e20949fe4840eacc4e15b065472393398b43342
Florence Navet <enolaworld@gmail.com>
2025-06-14 17:52:34 +0200

    fin cours format chaines

--- Cours/Bases/chaines.cs
+++ Cours/Bases/chaines.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using System.Text;
 
 namespace Chaines
@@ -7,6 +8,7 @@ namespace Chaines
     {
         public void AfficherChaines()
         {
+            Console.OutputEncoding = Encoding.UTF8;
             string s1 = "abc"; // la plus commune
 
             string s2 = new string('*', 7); // N fois la  meme chaine
@@ -14,6 +16,62 @@ namespace Chaines
             char[] ar = { 'a', 'b', 'c', };
             
             string s3 = new string(ar);
+
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("formatage avec ToSTring");
+            decimal prix = 3.5m;
+            double temp = 9.8;
+            Console.WriteLine(temp.ToString("N1"));
+            Console.WriteLine(DateTime.Today.ToString());
+            Console.WriteLine(prix.ToString("N3")); //3,500
+            Console.WriteLine(prix.ToString("C2"));  // 3,50€
+            Console.WriteLine(prix.ToString("C2", CultureInfo.GetCultureInfo("en-US"))); // 3,50$
+            Console.WriteLine(prix.ToString("N2", CultureInfo.InvariantCulture)); //
+            Console.WriteLine(prix.ToString("C2", CultureInfo.InvariantCulture)); //
+            Console.WriteLine();
+            long siren = 123456789;
+            Console.WriteLine(siren.ToString("000 000 000"));//123 456 789
+            Console.WriteLine();
+            long siren2 = 12345678;
+            Console.WriteLine(siren2.ToString("### ### ###"));//12 345 678
+            Console.WriteLine();
+            long siren3 = 12345678;
+            Console.WriteLine(siren3.ToString("000 000 000"));//012 345 678
+
+            Console.WriteLine();
+
+            double d = 2.4587;
+            Console.WriteLine(d.ToString("###.###"));//2.459
+            Console.WriteLine(d.ToString("00.0%"));//245.9%
+
+            Console.WriteLine("------------------------------");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Formats de date");
+
+            DateTime date = new DateTime(2030, 07, 14, 13, 58, 35); //14/07/2030 à 13:58:35
+
+            Console.WriteLine(date.ToString("d")); // date courte
+            Console.WriteLine(date.ToString("D")); // date longue
+            Console.WriteLine(date.ToString("MM/yyyy")); // date longue y, mois année.....
+            Console.WriteLine(date.ToString("ddd dd/MMM-yyyy à HH:mm z")); // 2 lettres = nombres - 3 l = text abreges - 4l =  text entier
+
+            Console.WriteLine("------------------------------");
+
+
+            Console.WriteLine();
+            Console.WriteLine("Formatage des duree");
+            TimeSpan duree = new TimeSpan(40, 37, 28); // 40h 37min 28s
+            Console.WriteLine(duree.ToString());//1:16:37:28
+            Console.WriteLine(duree.ToString("G"));//1:16:37:28,0000000
+            Console.WriteLine(duree.ToString("g"));//1:16:37:2
+            Console.WriteLine(duree.ToString("d'j 'hh'h 'mm'min 'ss's '"));
+   
+
+
+
+
 
             Console.WriteLine(s1);
             Console.WriteLine(s2);
--- Exercise/ExercicesCS/RelevesMeteo/Program.cs
+++ Exercise/ExercicesCS/RelevesMeteo/Program.cs
@@ -66,32 +66,7 @@ namespace RelevesMeteo
                 Console.WriteLine("Aucune température moyenne valide trouvée.");
             }
 
-            //double SommeTemp = 0;
-
-
-            //int NbValeurs = 0;
-
-            //for (int i = 1; i < lignes.Length; i++)
-            //{
-            //    string[] valeurs = lignes[i].Split(';');
-            //    if (double.TryParse(valeurs[2], out double tMin) && double.TryParse(valeurs[3], out double tMax))
-            //    {
-            //        SommeTemp += tMin + tMax;
-            //        NbValeurs += 2;
-            //    }
-            //    string resultat = AfficheData(valeurs);
-
-            //    Console.WriteLine(resultat);
-            //}
-            //if (NbValeurs > 0)
-            //{
-            //    double moyenne = SommeTemp / NbValeurs;
-            //    Console.WriteLine($"Température moyenne égale : {moyenne}");
-
-            //} else
-            //{
-            //    Console.WriteLine("Aucune température valide trouvée.");
-            //}
+            
         }
        
 
