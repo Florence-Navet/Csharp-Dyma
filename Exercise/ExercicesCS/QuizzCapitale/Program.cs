f7df59eb569be63821069311ad5745ae81c75a99
Florence <enolaworld@gmail.com>
2025-06-15 21:59:28 +0200

    feat: arrangement menu en ajoutement fin de jeu

--- Exercise/ExercicesCS/QuizzCapitale/Program.cs
+++ Exercise/ExercicesCS/QuizzCapitale/Program.cs
@@ -8,60 +8,81 @@ namespace MonApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Bienvenue dans le jeu de Quizz !");
+            bool continuer = true;
 
             //var numeros = Quizz.Generer3Numeros();
             //Console.WriteLine($"Numéros générés : {numeros.Item1}, {numeros.Item2}, {numeros.Item3}");
             //Quizz.Jouer(numeros.Item1, numeros.Item2, numeros.Item3);
 
+            while (continuer) 
+            {
+                Console.WriteLine("Souhaites-tu :");
+                Console.WriteLine("1 - Saisir 3 numéros de questions");
+                Console.WriteLine("2 - Obtenir 3 numéros aléatoires");
+                Console.WriteLine("3 - Faire tout le quiz");
+                Console.Write("Ton choix : ");
+                string choix = Console.ReadLine().ToUpper();
 
-            Console.WriteLine("Souhaites-tu :");
-            Console.WriteLine("1 - Saisir 3 numéros de questions");
-            Console.WriteLine("2 - Obtenir 3 numéros aléatoires");
-            Console.WriteLine("3 - Faire tout le quiz");
-            Console.Write("Ton choix : ");
-            string choix = Console.ReadLine().ToUpper();
+                (int n1, int n2, int n3) numeros;
+                bool rejouer = true;
 
-            (int n1, int n2, int n3) numeros;
-            bool rejouer = true;
-
-            while (rejouer) {
-
-                if (choix == "1")
+                while (rejouer)
                 {
-                    numeros = Quizz.Saisir3Numeros();
-                    Console.WriteLine($"Numéros sélectionnés : {numeros.n1}, {numeros.n2}, {numeros.n3}");
-                    Quizz.Jouer(numeros.n1, numeros.n2, numeros.n3);
-                    rejouer = Quizz.DemanderRejouer();
 
-                }
-                else if (choix == "2")
-                {
-                    numeros = Quizz.Generer3Numeros();
-                    Console.WriteLine($"Numéros générés : {numeros.n1}, {numeros.n2}, {numeros.n3}");
-                    Quizz.Jouer(numeros.n1, numeros.n2, numeros.n3);
-                    rejouer = Quizz.DemanderRejouer();
-
-
-                }
-                else if (choix == "3")
-                {
-                    int totalQuestions = 10;
-                    for (int i = 0; i < totalQuestions; i++)
+                    if (choix == "1")
                     {
-                        Quizz.Jouer(); 
+                        numeros = Quizz.Saisir3Numeros();
+                        Console.WriteLine($"Numéros sélectionnés : {numeros.n1}, {numeros.n2}, {numeros.n3}");
+                        Quizz.Jouer(numeros.n1, numeros.n2, numeros.n3);
                         rejouer = Quizz.DemanderRejouer();
 
                     }
+                    else if (choix == "2")
+                    {
+                        numeros = Quizz.Generer3Numeros();
+                        Console.WriteLine($"Numéros générés : {numeros.n1}, {numeros.n2}, {numeros.n3}");
+                        Quizz.Jouer(numeros.n1, numeros.n2, numeros.n3);
+                        rejouer = Quizz.DemanderRejouer();
+
+
+                    }
+                    else if (choix == "3")
+                    {
+                        int totalQuestions = 10;
+                        for (int i = 0; i < totalQuestions; i++)
+                        {
+                            Quizz.Jouer();
+                            rejouer = Quizz.DemanderRejouer();
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Choix invalide !! Veuillez saisir 1, 2 ou 3");
+                        break;
+                    }
+                    Console.WriteLine("\nRejouer dans le même mode de jeu ? o/n : ");
+                    string reponseRejouer = Console.ReadLine().Trim().ToLower();
+                    rejouer = (reponseRejouer == "o");
+
+
+                    if (!rejouer)
+                    {
+                        Console.WriteLine("Retourner au menu principal ? o/n :");
+                        string reponseMenu = Console.ReadLine().Trim().ToLower();
+                        continuer = (reponseMenu == "o");
+                        break;
+
+                    }
+             
+
 
                 }
-                else
-                {
-                    Console.WriteLine("Choix invalide !! Veuillez saisir 1, 2 ou 3");
-                }
-
-
-
             }
+
+
+        
 
           
 
