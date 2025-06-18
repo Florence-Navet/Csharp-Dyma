8281856620bf308e089d6fb23ffd9aad790c9c66
Florence Navet <enolaworld@gmail.com>
2025-06-18 18:00:35 +0200

    feat: fin exercice concours

---
+++ Exercise/ExercicesCS/Concours/Notation.cs
@@ -0,0 +1,41 @@
+﻿using Concours;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Concours
+{
+    public enum Mention { E, P, AB, B, TB }
+    internal class Notation
+    {
+        private static string[] Libelles = { "Échec", "Passable", "Assez bien", "Bien", "Très bien" };
+
+        public static (Mention, string) GetMention(double note)
+        {
+            Mention mention;
+
+            if (note < 10)
+                mention = Mention.E;
+            else if (note < 12)
+                mention = Mention.P;
+            else if (note < 14)
+                mention = Mention.AB;
+            else if (note < 16)
+                mention = Mention.B;
+            else
+                mention = Mention.TB;
+
+            return (mention, Libelles[(int)mention]);
+
+
+
+        
+
+        }
+
+    }
+}
+
---
+++ Exercise/ExercicesCS/Concours/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using Concours;
+
+namespace Concours
+{
+    internal class Program
+    {
+        public static (string nomComplet, double moyenne)[] Etudiants;
+
+        static void Main()
+        {
+            Console.WriteLine("Résultat Concours Étudiant !");
+            string cheminFichier = "Etudiants.csv";
+
+            if (File.Exists(cheminFichier))
+            {
+                ChargerDonnees(cheminFichier);
+                AfficherEtudiants();
+                Console.WriteLine();
+                Console.WriteLine("Résultats du concours :\n");
+                AfficherResultatsConcours(cheminFichier);
+            }
+            else
+            {
+                Console.WriteLine("Fichier introuvable !");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void ChargerDonnees(string chemin)
+        {
+            string[] lignes = File.ReadAllLines(chemin);
+            Etudiants = new (string, double)[lignes.Length - 1];
+
+            for (int i = 1; i < lignes.Length; i++) // Commencer à 1 pour sauter l'en-tête
+            {
+                string[] infos = lignes[i].Split(';');
+                string nomComplet = infos[1] + " " + infos[0]; // Prénom Nom
+                double moyenne = double.Parse(infos[4].Replace(',', '.'), CultureInfo.InvariantCulture);
+
+                Etudiants[i - 1] = (nomComplet, moyenne);
+            }
+        }
+
+        public static void AfficherEtudiants()
+        {
+            Console.WriteLine("Liste des étudiants :\n");
+
+            foreach (var (nom, moyenne) in Etudiants)
+            {
+                Console.WriteLine($"{nom} : {moyenne} / 20");
+            }
+        }
+
+        public static void AfficherResultatsConcours(string cheminFichier)
+        {
+            if (!File.Exists(cheminFichier))
+            {
+                Console.WriteLine($"Le fichier {cheminFichier} est introuvable.");
+                return;
+            }
+
+            string[] lignes = File.ReadAllLines(cheminFichier);
+
+            
+
+            foreach (var (nom, moyenne) in Etudiants)
+            {
+                var mention = Notation.GetMention(moyenne);
+                string res = moyenne >= 10 ? "Admis" : "";
+                Console.WriteLine($"{nom,-20} : {moyenne,5:N1}  {mention.Item2,-12} {res}");
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
