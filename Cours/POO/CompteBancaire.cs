ce976282267b833e824e43e3b1d0293076c7afab
Florence Navet <enolaworld@gmail.com>
2025-06-20 14:36:09 +0200

    feat: fin cours propieté

--- Cours/POO/CompteBancaire.cs
+++ Cours/POO/CompteBancaire.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,14 +10,23 @@ namespace POO
     internal class CompteBancaire
     {
         #region Champs privés (invisibles en dehors de la classe) ~~   #region Champs privés (accessibles uniquement à l’intérieur de la classe)~~
-        private long _numero;
+        private readonly long _numero;
         private decimal _solde;
+        private const decimal TAUX_AGIOS = 0.15m;
+        public static readonly decimal TAUX_INTERET = 0.2m; //mieux pour les public
+        #endregion
+
+        #region Champs publics (accessibles de partout)
+        public string Libelle;
         #endregion
 
         // Contructeur
         public CompteBancaire(long num)
         {
             _numero = num; // Initialise le numéro du compte
+            Libelle = "Compte N°" + num;
+
+           
         }
 
         #region Méthodes publiques
@@ -30,12 +40,22 @@ namespace POO
         public void Debiter(decimal montant)
         {
             _solde -= montant;
+            if (_solde < 0)
+                _solde -= _solde * TAUX_AGIOS;
         }
 
         // Renvoie la valeur du solde courant
         public decimal GetSolde()
         {
             return _solde;
+        }
+        #endregion
+        #region Méthodes protégées (accessibles à l’intérieur de la classe et de ses dérivées)
+        protected virtual decimal CalculerInterets()
+        {
+            decimal interets = _solde * 0.005m;
+            _solde += interets;
+            return interets;
         }
         #endregion
     }
--- Cours/POO/Program.cs
+++ Cours/POO/Program.cs
@@ -12,14 +12,31 @@ namespace POO
             // Instancie un compte bancaire en sépcifiant son numéro
             long numero = 123456; //long numero = 123456~~
             CompteBancaire compte1 = new CompteBancaire(numero);
+            // Instancie un compte bancaire et initialise son numéro
+            long numero2 = 8456122;
+            CompteBancaire compte2 = new CompteBancaire(numero2);
+
+
+            // Les 2 lignes suivantes provoqueraient des erreurs,
+            // car elles tentent d'accéder à des membres qui ne sont pas publics
+            //compte1._solde = 1000m;
+            //decimal i = compte2.CalculerInterets();
+
+
+            // Les lignes suivantes sont OK, car elles utilisent des membres publics
+            compte2.Libelle = "Compte d'épargne";
+            compte2.Crediter(500);
+            compte2.Debiter(60);
+            decimal solde = compte2.GetSolde();
 
             // Utilise le compte en appelant ses méthodes publiques
-            compte1.Crediter(500m);
+            compte1.Crediter(800m);
             compte1.Debiter(60m);
-            decimal solde = compte1.GetSolde();
+            decimal solde1 = compte1.GetSolde();
 
             // Récupère le solde courant via une méthode publique
             Console.WriteLine($"Le solde du compte {numero} est de {solde:C2}");
+            Console.WriteLine($"Le solde du compte {numero2} est de {solde1:C2}");
         }
     }
 }
