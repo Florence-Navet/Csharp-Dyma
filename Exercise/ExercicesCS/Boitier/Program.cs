a276a98021cae76410b28ec8e9c48b3d4fd168b7
AeRiaSaito <florence.navet@laplateforme.io>
2025-06-23 14:15:17 +0200

    feat: debut exercice lecon 54 #22

--- Exercise/ExercicesCS/Boitier/Program.cs
+++ Exercise/ExercicesCS/Boitier/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Job sur les Boites");
+            //1.instancier une boite
+            Boite maBoite = new Boite();
+
+            //2.Affiche volume et matiere de la boite
+            Console.WriteLine($"Le volume de la boite est de {maBoite.Volume} cm3");
+            Console.WriteLine($"La matiere de la boite est en {maBoite.Matiere}");
+
+
+
+            //3.appeler sa methode Etiqueter
+            maBoite.Etiqueter("Adeline Patenne", true);
+
+            //4.afficher si contenu fragile ou pas
+            Console.WriteLine($"Destinataire : {maBoite.Destinataire}");
+            Console.WriteLine($"Contenu fragile : {(maBoite.Fragile ? "Oui" : "Non")}");
         }
     }
 }
