fbaa65e2fe2a7d7683474d23271084c2df44eebe
Florence <enolaworld@gmail.com>
2025-06-23 17:35:30 +0200

    feat: exercice partie 1 #22

--- Exercise/ExercicesCS/Boitier/Boite.cs
+++ Exercise/ExercicesCS/Boitier/Boite.cs
@@ -16,28 +16,66 @@ namespace Boitier
         Metal
     }
     internal class Boite
+
     {
+
+        #region Champs et propriétés statiques
+
+        
+
+        // Propriété statique en lecture seule
+        //public static int NbBoites => _nbBoites;
+
+        #endregion
+
+
+        #region Constructeurs
+
+        public Boite(double hauteur, double largeur, double longueur)
+        {
+            Hauteur = hauteur;
+            Largeur = largeur;
+            Longueur = longueur;
+            NbBoites++;
+        }
+
+        public Boite(double hauteur, double largeur, double longueur, Matieres matiere) : 
+            this(hauteur, largeur, longueur)
+        {
+            Matiere = matiere;
+        }
+        #endregion
+
+        #region Proprietes
+
+
         //juste en lecteur donc juste le get
 
-        public double Hauteur { get; } = 30.0;
+        public double Hauteur { get; set; } = 30.0;
         public double Largeur { get; } = 30.0;
         public double Longueur { get; } = 30.0;
+
+        public static int NbBoites { get; private set; }
+
+
+        public Matieres Matiere { get; } = Matieres.Carton;
+     
 
         //propriété Volume en lecture qui retourne le volume
         public double Volume => Hauteur * Largeur;
         // Champ privé associé à la propriété en lecture seule
         private string _destinataire = string.Empty;
 
-      
-
-        //Propriete en lecture seule initialisé à carton
-        public Matieres Matiere { get; } = Matieres.Carton;
 
         //Créer une propriete Destinaire de type string en lecture seule
         public string Destinataire { get; private set; } = string.Empty;
         //créer une propriete fragile en lecture seule
         public bool Fragile { get; private set; }
 
+        #endregion
+
+
+        #region Methodes publiques
 
         //Créer une methode publique Etiqueter permettant
         //d'affecter la valeur de la propriete Destinataire
@@ -51,11 +89,7 @@ namespace Boitier
             Fragile = f;
         }
 
-
-        //créer ensuite une surchage permettant d'affectant
-        //les valeurs des proprietes Destinataires et Fragile
-
-
+        #endregion
 
 
     }
--- Exercise/ExercicesCS/Boitier/Program.cs
+++ Exercise/ExercicesCS/Boitier/Program.cs
@@ -6,21 +6,51 @@
         {
             Console.WriteLine("Job sur les Boites");
             //1.instancier une boite
-            Boite maBoite = new Boite();
+            Boite maBoite = new Boite(15,12,10);
+            Console.WriteLine($"Nombre total de boites crées : {Boite.NbBoites}");
+
 
             //2.Affiche volume et matiere de la boite
             Console.WriteLine($"Le volume de la boite est de {maBoite.Volume} cm3");
             Console.WriteLine($"La matiere de la boite est en {maBoite.Matiere}");
 
+            Boite maBoite1 = new Boite(15, 10, 10, Matieres.Plastique);
+            Console.WriteLine($"Nombre total de boites crées : {Boite.NbBoites}");
+            Console.WriteLine($"Le volume de la boite est de {maBoite1.Volume} cm3");
+            Console.WriteLine($"La matiere de la boite est en {maBoite1.Matiere}");
+
+            Boite maBoite2 = new Boite(10, 18, 20);
+            Console.WriteLine($"Nombre total de boites crées : {Boite.NbBoites}");
+            Console.WriteLine($"Le volume de la boite est de {maBoite2.Volume} cm3");
+           
+
+
+
 
 
             //3.appeler sa methode Etiqueter
             maBoite.Etiqueter("Adeline Patenne", true);
+            maBoite1.Etiqueter("Anya Spy X Family", false);
 
             //4.afficher si contenu fragile ou pas
             Console.WriteLine($"Destinataire : {maBoite.Destinataire}");
             Console.WriteLine($"Contenu fragile : {(maBoite.Fragile ? "Oui" : "Non")}");
-            Console.WriteLine($"Boîte destinée à {b1.Destinataire}, contenu {(maBoite.Fragile ? "fragile" : "non fragile")}");
+            Console.WriteLine($"Boîte destinée à {maBoite.Destinataire}, " +
+                $"contenu {(maBoite.Fragile ? "fragile" : "non fragile")}");
+           
+
+            Console.WriteLine($"Destinataire : {maBoite1.Destinataire}");
+            Console.WriteLine($"Contenu fragile : {(maBoite1.Fragile ? "Oui" : "Non")}");
+            Console.WriteLine($"Boîte destinée à {maBoite1.Destinataire}, " +
+                $"contenu {(maBoite.Fragile ? "fragile" : "non fragile")}");
+            
+
+            Console.WriteLine($"Destinataire : {maBoite2.Destinataire}");
+            Console.WriteLine($"Contenu fragile : {(maBoite2.Fragile ? "Oui" : "Non")}");
+            Console.WriteLine($"Boîte destinée à {maBoite2.Destinataire}, " +
+                $"contenu {(maBoite.Fragile ? "fragile" : "non fragile")}");
+
+            
         }
     }
 }
