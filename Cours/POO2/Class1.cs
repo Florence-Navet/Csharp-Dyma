843d82a165a40708c88306a90b1051a90bb55ffd
Florence <enolaworld@gmail.com>
2025-06-24 17:19:52 +0200

    feat: leçon 57 et 58 du chap 8 #25

---
+++ Cours/POO2/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.Data.SqlClient;
+using System.Xml.Serialization;
+using System.Threading.Channels;
+using static System.Math;
+
+
+namespace POO2
+{
+    internal class Class1
+    {
+        public static void TesterNS()
+        {
+            Console.WriteLine();
+
+            StringBuilder sb;
+
+            XmlSerializer xs;
+        }
+     
+        public static double GetDistance(double x1, double y1, double x2, double y2)
+        {
+            return Sqrt(Pow(y2 - y1, 2) + Pow(x1 - x2, 2));
+        }
+    }
+}
+    
+   
