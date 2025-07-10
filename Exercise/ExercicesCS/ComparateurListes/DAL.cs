e6845563ec37e0faa6cd742a6459afc0fbac9577
Florence Navet <enolaworld@gmail.com>
2025-07-10 19:58:13 +0200

    feat: comparateurlistes

---
+++ Exercise/ExercicesCS/ComparateurListes/DAL.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComparateurListes
+{
+    internal class DAL
+    {
+        //comparaison sur les propriétés des objets
+        public record class Etudiant(string Nom, string Prenom)
+        {
+
+            public override string ToString()
+            {
+                return Nom + " " + Prenom;
+            }
+        }
+
+
+        //comparaisson sur les references d'objets
+        /*public class Etudiant
+        {
+            public Etudiant(string nom, string prénom)
+            {
+                Nom = nom;
+                Prénom = prénom;
+            }
+
+            public string Nom { get; }
+            public string Prénom { get; }
+
+            public override string ToString()
+            {
+                return Nom + " " + Prénom;
+            }
+
+        }*/
+
+            public static HashSet<Etudiant> GetEtudiants(string nomFichier)
+        {
+            HashSet<Etudiant> étudiants = new();
+            string[] lignes = File.ReadAllLines(nomFichier);
+
+            for (int i = 0; i < lignes.Length; i++)
+            {
+                string[] infos = lignes[i].Split(';');
+                étudiants.Add(new Etudiant(infos[0], infos[1]));
+            }
+            return étudiants;
+        }
+    }
+}
