76c7790da00b2f71b26a772cb632602beb54bfcc
Florence Navet <enolaworld@gmail.com>
2025-07-20 20:18:41 +0200

    feat: exercice methode extension : #64

---
+++ Exercise/ExercicesCS/ClassesEtendues/DateTimeExtension.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesEtendues
+{
+    public static class DateTimeExtension
+    {
+        /// <summary>
+        /// Renvoie une date correspond au premier jour du mois de la date
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns>premiere jour du mois de la date</returns>
+        public static DateTime GetBeginnigOfTheMonth(this DateTime date)
+        {
+            return new DateTime(date.Year, date.Month, 1);
+        }
+
+
+        /// <summary>
+        /// Renvoie une date correspondant au dernier jour du mois
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static DateTime GetEndOfTheMonth(this DateTime date)
+        {
+            var endOfTheMonth = new DateTime(date.Year, date.Month, 1)
+            .AddMonths(1)
+            .AddDays(-1);
+       
+
+            return endOfTheMonth;
+        }
+
+        /// <summary>
+        /// renvoie le nb d'années écoulées entre date et date jour
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static int GetAge (this DateTime date)
+        {
+            int ans = DateTime.Today.Year - date.Year;
+            if (DateTime.Today.Month < date.Month ||
+                DateTime.Today.Month == date.Month && DateTime.Today.Day < date.Day)
+                ans--;
+
+            return ans;
+
+        }
+
+        public static DateTime AddWorkedDays(this DateTime date, int nbDays)
+        {
+            int addDays = 0;
+            DateTime currentDate = date;
+            int direction;
+
+            if (nbDays >= 0)
+            {
+                direction = 1;
+            }
+            else
+            {
+                direction = -1;
+            }
+
+            while (currentDate.DayOfWeek == DayOfWeek.Saturday || currentDate.DayOfWeek == DayOfWeek.Sunday)
+            {
+                currentDate = currentDate.AddDays(direction);
+            }
+
+            while (addDays < Math.Abs(nbDays))
+            {
+                currentDate = currentDate.AddDays(direction);
+
+                if (currentDate.DayOfWeek != DayOfWeek.Saturday &&
+                    currentDate.DayOfWeek != DayOfWeek.Sunday)
+                {
+                    addDays++;
+                }
+            }
+            return currentDate;
+        }
+
+    }
+}
---
+++ Exercise/ExercicesCS/ClassesEtendues/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassesEtendues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("======CLASSES ETENDUES=======");
+
+            // dt0 = new DateTime(2001, 04, 15);
+            //DateTime dt0 = new DateTime(2025, 07, 20);
+            DateTime dt0 = new DateTime(2000, 12, 25);
+
+            Console.WriteLine($"Date de départ :  {dt0:D}");
+            Console.WriteLine($"Premier jour du mois :  {dt0.GetBeginnigOfTheMonth():D}");
+            Console.WriteLine($"Dernier jour du mois :  {dt0.GetEndOfTheMonth():D}");
+            Console.WriteLine();
+            Console.WriteLine($"Nombre d'années entières entre la date de départ le {dt0:D}" +
+                $" et le {DateTime.Today:d} : {dt0.GetAge()}");
+            Console.WriteLine();
+            Console.WriteLine($"Départ : {dt0:dddd dd MMM yyyy}");
+            Console.WriteLine($"+ 5 jours ouvrés : {dt0.AddWorkedDays(5):dddd dd MMMM yyyy}");
+
+            Console.WriteLine();
+
+            foreach (Status stat in Enum.GetValues(typeof(Status)))
+            {
+                Console.WriteLine(stat.ToDisplayString());
+            }
+            
+
+         
+
+
+
+
+        }
+    }
+}
