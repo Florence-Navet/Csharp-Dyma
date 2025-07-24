7b1ca7f36aadb019fa0a3a18052ac4eb0ea27e0a
Florence Navet <enolaworld@gmail.com>
2025-07-24 13:54:01 +0200

    feat: exercice tableau periodique element #66

---
+++ Exercise/ExercicesCS/ClassificationPeriodique/Element.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassificationPeriodique
+{
+    public record class Famille
+    {
+        public required short Id { get; init; }
+        public required string Nom { get; init; }
+        public string? Couleur { get; init; }
+    }
+
+    public record class Etat
+    {
+        public required char Code { get; init; }
+        public required string Nom { get; init; }
+        public string? Couleur { get; init; }
+    }
+
+    public record class Element
+    {
+        public short Période { get; init; }
+        public required short NumAtomique { get; init; }
+        public required string Symbole { get; init; }
+        public required char CodeEtat { get; init; }
+
+        public short NumFamille { get; init; }
+
+        public required string Nom { get; init; }
+    }
+}
---
+++ Exercise/ExercicesCS/ClassificationPeriodique/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
+
+namespace ClassificationPeriodique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Classification Périodique des éléments");
+
+            TableauPeriodique.ChargerFichierTxt();
+
+            //short num1 = 35;
+            //char etat = TableauPeriodique.Elements[num1].CodeEtat;
+
+            //ConsoleColor couleur = TableauPeriodique.GetCouleurEtat(etat);
+            //Console.ForegroundColor = couleur;
+            //Console.WriteLine($"Élément n°{num1} → Couleur d’état : {couleur}");
+            //Console.ResetColor();
+
+
+            //TesterCouleurs();
+
+
+
+            while (true)
+            {
+                short num = SaisirNuméro();
+
+
+                //Console.WriteLine(TableauPeriodique.GetDetailElement(num));
+
+
+                //TableauPeriodique.AfficherDetailElement(num);
+                ConsoleColor couleurOrig = Console.ForegroundColor;
+                Console.ForegroundColor = TableauPeriodique.GetCouleurEtat(TableauPeriodique.Elements[num].CodeEtat);
+                Console.WriteLine(TableauPeriodique.GetDetailElement(num));
+                Console.ForegroundColor = couleurOrig;
+
+
+
+
+                Console.WriteLine("\nAppuyez sur Echap pour quitter ou n'importe quelle autre touche pour continuer.");
+                if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+                    Environment.Exit(0);
+                else
+                    Console.Clear();
+
+            }
+        }
+
+        public static short SaisirNuméro() 
+        {
+            bool saisieOK = false;
+
+            short num = 1;
+       
+         
+
+            while (!saisieOK)
+            {
+                Console.WriteLine("Numéro atomique de l'élément :");
+                string? saisie = Console.ReadLine();
+
+                if (short.TryParse(saisie, out num))
+                {
+                    if (num >= 1 && num <= 118)
+                        saisieOK = true;
+
+                    else
+                        Console.WriteLine("Veuillez entrer un nombre entre 1 et 118");
+                }
+                else
+                {
+                    Console.WriteLine("Format non valide. Veuillez entrer un nombre entier.");
+                }
+                   
+            }
+            return num;
+        }
+        public static void TesterCouleurs()
+        {
+            TestEtat(34, 'S');
+            TestEtat(35, 'L');
+            TestEtat(36, 'G');
+            TestEtat(109, '?');
+            Console.ResetColor();
+        }
+
+        public static void TestEtat(int numero, char etat)
+        {
+            ConsoleColor couleur = TableauPeriodique.GetCouleurEtat(etat);
+            Console.ForegroundColor = couleur;
+            Console.WriteLine($"Élément n°{numero} - État : {etat} → Couleur : {couleur}");
+        }
+
+
+
+    }
+}
